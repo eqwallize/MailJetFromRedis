eb879a43deed1eba85503602735f389db84537e6
Stephane <stephane@eqwall.com>
2015-03-10 21:38:50 +0100

    Fix trouble when redis crash

--- main.js
+++ main.js
@@ -8,41 +8,53 @@ var config = require('./config.json');
 var API_KEY = config.mailjet.api_key;
 var API_SECRET = config.mailjet.api_secret;
 
-var connectRedis = function(){
-   try{
-    	var port = process.env.REDIS_PORT_6379_TCP_PORT;
-    	var host = process.env.REDIS_PORT_6379_TCP_ADDR;    
-    	redisSub = redisLib.createClient(port, host);
-    	redis = redisLib.createClient(port, host);
-   } catch(e){
-    	console.log("error connecting to redis" + e);
-   	redisSub = redisLib.createClient();
-    	redis = redisLib.createClient();
-   }
-    manageRedisSub();
+//Redis info
+var port = process.env.REDIS_PORT_6379_TCP_PORT;
+var host = process.env.REDIS_PORT_6379_TCP_ADDR;
+
+var connectRedisSub = function(){
+    console.log("connect");
+    try {
+        //if env var exist, user it
+        if(port && host){
+            redisSub = redisLib.createClient(port, host);
+        } else {
+            //else give a try to default
+            redisSub = redisLib.createClient();
+        }
+    }catch(e){
+        console.log("couldn't connect to redis Sub", e);
+    }
+    if(redisSub){ 
+        manageRedisSub();
+    }
 };
 
-
+/** manage redis pub/sub. */
 var manageRedisSub = function(){
-    redisSub.subscribe(KEY_SUBSCRIBE_REDIS_MAIL_SEND);
     redisSub.on("message", function(channel, message) { 
         console.log('subscribe ' + " channel: "+ channel + " msg: "+ message);    
-	   if(channel == KEY_SUBSCRIBE_REDIS_MAIL_SEND){
+        if(channel == KEY_SUBSCRIBE_REDIS_MAIL_SEND){
            msg = JSON.parse(message);
            
             mailLib.sendContent(msg.from, msg.destinations,
                 msg.subject, msg.type, msg.content);
-       }
+       }       
     });
-
+    redisSub.on("connect", function(message){
+        redisSub.subscribe(KEY_SUBSCRIBE_REDIS_MAIL_SEND);
+    });    
     redisSub.on("error", function(message){
         //Deal with error
-        //TODO add a count to avoid infinite loop
-        connectRedis();    
-        console.log('error redis redis disconnected ' + message);    
+        console.log('error redis disconnected ' + message);    
+    });
+    
+    redisSub.on("end", function(message){
+        //Deal with error
+        console.log('error redis end ' + message);    
     });
 };
 
 mailLib = new Mailjet (API_KEY, API_SECRET);
-	    
-connectRedis();
+
+connectRedisSub();
